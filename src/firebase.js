bbd2e0279489f2ffe8786ff46ff0e86aa8e94781
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-07-12 14:50:29 +0300

    feat: add firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA97Y5iSPCRxbppuOa2FeOnlWqdg3SbbqU",
+  authDomain: "r-chat-36db0.firebaseapp.com",
+  projectId: "r-chat-36db0",
+  storageBucket: "r-chat-36db0.appspot.com",
+  messagingSenderId: "253307094176",
+  appId: "1:253307094176:web:8af76450464fb64f32b5c5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
